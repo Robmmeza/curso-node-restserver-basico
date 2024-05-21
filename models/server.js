91286e3d184b4b49295ae59cef1671886f0a98f8
Roberto Martinez <rob199125mm@outlook.com>
2024-05-21 09:09:04 -0600

    Primer Commit

---
+++ models/server.js
@@ -0,0 +1,52 @@
+
+const express = require('express');
+const cors = require('cors');
+
+class Server {
+
+
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+        this.usuariosPath = '/api/usuarios';
+
+        //Middlewares
+        this.middleWares();
+
+        //Rutas de la app
+        this.routes();
+    }
+
+    middleWares() {
+
+        //CORS
+
+        this.app.use( cors() );
+
+        //Lectura y parseo de Body
+
+        this.app.use( express.json() );
+
+        // Directorio public
+        this.app.use( express.static('public') );
+    }
+
+    routes() {
+
+        this.app.use(this.usuariosPath, require('../routes/usuarios'));
+        
+    }
+
+
+    listen() {
+
+        this.app.listen( this.port, () => {
+            console.log('Servidor corriendo en puerto: ', this.port );
+        } );
+    }
+
+
+
+}
+
+module.exports = Server;
